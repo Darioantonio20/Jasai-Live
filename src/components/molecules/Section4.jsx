9c9dec9f86a8048de76e3c4381f496e750c4905c
DarioAntonio <antoniodario10@hotmail.com>
2023-09-15 21:20:53 -0600

    [ADD] Section4 carrusel de boletos

---
+++ src/components/molecules/Section4.jsx
@@ -0,0 +1,63 @@
+import { Link } from "react-router-dom";
+import BoletoImg1 from "../../assets/img/ImgBoleto1.png";
+import BoletoImg2 from "../../assets/img/ImgBoleto2.png";
+import IconoNext from "../../assets/img/iconoNext.png";
+import IconoPreview from "../../assets/img/iconoPreview.png";
+import "../../assets/styles/Pages.css";
+
+function Section4() {
+    return ( 
+        <>
+        <div id="carouselExample" class="carousel slide">
+            <div class="carousel-inner">
+                <div className="container-fluid align">
+                    <h2 className="TituloSection4 mb-5 mt-5">Explorar más eventos</h2>
+                    <div className="carousel-item active">
+                        <div className="row justify-content-center row-cols-1 row-cols-md-3 g-4">
+                            <div className="col">
+                                <div className="card h-100">
+                                    <img src={BoletoImg1} className="card-img-top" alt="Boletos"/>
+                                </div>
+                            </div>
+                            <div className="col">
+                                <div className="card h-100">
+                                    <img src={BoletoImg2} className="card-img-top" alt="Boletos"/>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="carousel-item active">
+                        <div className="row justify-content-center row-cols-1 row-cols-md-3 g-4 ">
+                            <div className="col">
+                                <div className="card h-100">
+                                    <img src={BoletoImg1} className="card-img-top" alt="Boletos"/>
+                                </div>
+                            </div>
+                            <div className="col">
+                                <div className="card h-100">
+                                    <img src={BoletoImg2} className="card-img-top" alt="Boletos"/>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+                    <img src={IconoPreview} aria-hidden="true"></img>
+                    <span class="visually-hidden">Previous</span>
+                </button>
+                <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+                    <img src={IconoNext}  aria-hidden="true"></img>
+                    <span class="visually-hidden">Next</span>
+                </button>
+            </div>
+            <div className="d-flex justify-content-center m-5">
+                <Link to="/visualizacionBoletos">
+                    <button type="button" className="btnVerMas">Ver más</button>
+                </Link>
+            </div>
+        </div>
+        </>
+     );
+}
+
+export default Section4;
--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -3,6 +3,7 @@ import NavBar from "../components/atoms/NavBar";
 import Carrousel from "../components/molecules/Carrousel";
 import Section2 from "../components/molecules/Section2";
 import Section3 from "../components/molecules/Section3";
+import Section4 from "../components/molecules/Section4";
 
 function LandingPage() {
     return ( 
@@ -11,6 +12,7 @@ function LandingPage() {
            <Carrousel/>
            <Section2/>
            <Section3/>
+           <Section4/>
            <Footer/>
         </>
      );

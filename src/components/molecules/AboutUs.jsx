675a33b2f3a6d1b7cc0cac27c68f0827c2060058
DarioAntonio <antoniodario10@hotmail.com>
2023-09-20 08:19:13 -0600

    [FIX] about us seccion

--- src/components/molecules/AboutUs.jsx
+++ src/components/molecules/AboutUs.jsx
@@ -1,24 +1,28 @@
-import CardNanio from "../../assets/img/cardNanioAboutUs.png";
-import CardLic from "../../assets/img/cardLicenciadoAboutUs.png";
-import CardDoc from "../../assets/img/cardDocCarlosAboutUs.png";
+import DarioFoto from "../../assets/img/darioFoto.png";
+import DavidFoto from "../../assets/img/imgLicCarlos.jpeg";
+import CarlosFoto from "../../assets/img/imgLicDavid.jpeg";
 import "../../assets/styles/Pages.css";
 
 function AboutUs() {
     return ( 
         <>
-        <h2 className="TituloSection4 mb-5 mt-5">Equipo de desarrollo</h2>
-        <div className="container-fluid mt-5"><br/>
-            <div className="row">
-                <div className="col-sm-12 col-md-4 col-lg-4 col-xl-4 text-center">
-                    <img className="mx-auto imgResponsiva" src={CardNanio}></img>
-                </div>
-                <div className="col-sm-12 col-md-4 col-lg-4 col-xl-4 text-center">
-                    <img className="mx-auto imgResponsiva" src={CardLic}></img>
-                </div>
-                <div className="col-sm-12 col-md-4 col-lg-4 col-xl-4 text-center">
-                    <img className="mx-auto imgResponsiva" src={CardDoc}></img>
-                </div>
-            </div>
+        <h4 class="font-weight-bold text-center mb-5 mt-5">Equipo de desarrollo</h4>
+        <div class="row">
+          <div class="col-lg-4 text-center">
+            <img class="rounded-circle" src={DarioFoto} alt="Imagen Darío" width="140" height="140"/>
+            <h2>Darío Antonio Gutiérrez Álvarez</h2>
+            <p>Scrum Master</p>
+          </div>
+          <div class="col-lg-4 text-center">
+            <img class="rounded-circle" src={DavidFoto} alt="Generic placeholder image" width="140" height="140"/>
+            <h2>Jesús David Ruiz Garcia</h2>
+            <p>Scrum Team</p>
+          </div>
+          <div class="col-lg-4 text-center">
+            <img class="rounded-circle" src={CarlosFoto} alt="Generic placeholder image" width="140" height="140"/>
+            <h2>Carlos Ivan Cruz Zarmiento</h2>
+            <p>Product Owner</p>
+          </div>
         </div>
         </>
      );

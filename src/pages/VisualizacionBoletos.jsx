f5ab986aa51e998a1f82b85df564f3c3203c601c
DarioAntonio <antoniodario10@hotmail.com>
2023-09-26 21:43:54 -0600

    [FIX] formato de texto en boletos

--- src/pages/VisualizacionBoletos.jsx
+++ src/pages/VisualizacionBoletos.jsx
@@ -6,24 +6,62 @@ function VisualizacionBoletos() {
     return ( 
         <>
            <NavBar/>
-            <div className="row">
-                
-                <div className="col">
-                    <div className="col-md-6 col-sm-12 col-xl-6 colorBoleto">
+            <div className="row d-flex">
+                <div className="col-sm-12 col-md-6 col-xl-6 d-flex justify-content-md-end justify-content-sm-center">
+                    <div className="col-sm-12 col-md-6 col-xl-6 colorBoleto">
                         <div>
-                       
                             <div className="d-flex justify-content-between">
                                 <img src={IconoBlancoBoleto}></img>
-                                <div>
-                                    prueba
+                                <div className="text-center m-5">
+                                    <div className="text-light text-center">
+                                        Gran Cafe | Tuxtla Gutierrez, chiapas
+                                    </div>
+                                    <div className="text-light text-center">
+                                        café concert
+                                    </div>
+                                    <div className="d-flex justify-content-around text-light text-center">
+                                        <div>
+                                            Lugar:
+                                        </div>
+                                        <div>
+                                            Estadio panchon contreras tuxtla gutierrez, chiapas
+                                        </div>
+                                    </div>
+                                    <div className="d-flex justify-content-around text-light text-center">
+                                        <div>
+                                            Fecha:
+                                        </div>
+                                        <div>
+                                            Estadio panchon contreras tuxtla gutierrez, chiapas
+                                        </div>
+                                    </div>
+                                    <div className="d-flex justify-content-around text-light text-center">
+                                        <div>
+                                            Hora:
+                                        </div>
+                                        <div>
+                                            Estadio panchon contreras tuxtla gutierrez, chiapas
+                                        </div>
+                                    </div>
+                                    <div className="d-flex justify-content-around text-light text-center">
+                                        <div className="">
+                                            Localidad:
+                                        </div>
+                                        <div>
+                                            <div>Vip</div>
+                                            <div>Preferente</div>
+                                            <div>General</div>
+                                        </div>
+                                    </div>
+                                    <button type="submit" className="btnVerMas mb-4 mt-4">Comprar boleto</button>
                                 </div>
                                 <img className="rotaciónBoleto" src={IconoBlancoBoleto}></img>
                             </div>
                         </div>
                     </div>
                 </div>
-                <div className="col">
-                    <div className="col-md-6 col-sm-12 col-xl-6 colorBoleto">
+                <div className="col-sm-12 col-md-6 col-xl-6 d-flex justify-content-md-start justify-content-sm-center">
+                    <div className="col-sm-12 col-md-6 col-xl-6 colorBoleto">
                         <div>
                             <div className="d-flex justify-content-between">
                                 <img src={IconoBlancoBoleto}></img>
